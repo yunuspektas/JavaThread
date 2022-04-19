c0acf099250d24d8bae52869cc66f2ea5aa077e5
Yunus Pektas <yunuspektas@protonmail.com>
2022-04-19 20:26:22 +0300

    04.19.2022

---
+++ src/Example_01/deneme.java
@@ -0,0 +1,15 @@
+package Example_01;
+
+public class deneme {
+    public static void main(String[] args) {
+        String str1="Ali Can";
+        String str2=null;
+        System.out.println(str2);
+
+        System.out.println((str1+str2).length());
+
+
+        System.out.println(str2.length());
+
+    }
+}
